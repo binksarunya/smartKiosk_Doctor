e14ac54cc47c8fff42ec1218791462943584fdd1
binksarunya <binkrcy.solo@hotmail.com>
2018-05-27 23:18:01 +0700

    edit page

--- src/app/models/connect.ts
+++ src/app/models/connect.ts
@@ -1,9 +1,7 @@
 export class Connect {
-<<<<<<< HEAD
-=======
-   /* private static hostUrl = 'http://aehome.ddnsthailand.com/smart/doctor'; */
->>>>>>> e2c7ba452caec7a41ff2a4fdd38f77fa341ad1bf
-    private static hostUrl = 'http://localhost/smart/doctor';
+
+    private static hostUrl = 'http://aehome.ddnsthailand.com/smart/doctor'; 
+    /*private static hostUrl = 'http://localhost/smart/doctor';*/
     public static readonly USER_TOKEN = 'user_token';
     static getHostUrl():string {
       return this.hostUrl;
